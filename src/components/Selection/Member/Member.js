51e52e3916715f60c1e39166c814c289a278197d
Kayes-a2 <you@example.com>
2021-09-27 22:09:05 +0600

    done

--- src/components/Selection/Member/Member.js
+++ src/components/Selection/Member/Member.js
@@ -9,16 +9,19 @@ const Member = (props) => {
 
     return (
         //member part of html (jsx)
-        <div className="member">
+        <>
             <div className="member">
-                <img className="member-image" src={devloper.image} alt="" srcset="" />
-                <div className="details">
-                    <h4><li>{devloper.name}</li></h4>
-                    <h5><li>{devloper.designation}</li></h5>
+                <div className="member">
+                    <img className="member-image" src={devloper.image} alt="" srcset="" />
+                    <div className="details">
+                        <h4><li>{devloper.name}</li></h4>
+                        <h5><li>{devloper.designation}</li></h5>
+                    </div>
                 </div>
+
             </div>
             <hr />
-        </div>
+        </>
     );
 };
 
