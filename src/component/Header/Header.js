6164a90c3c6617dfd8bcc4c5d8f3a7e5537729e3
Kayes-a2 <you@example.com>
2021-09-27 17:25:44 +0600

    done

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCode } from '@fortawesome/free-solid-svg-icons';
+
 import './Header.css'
 
 const Header = () => {
     return (
         <div className="header-name">
-            <h1>Make your Devloper team</h1>
-            <h3>Pic your Devloper and Start the project with your team. Finish the work before deadline within the budget</h3>
-            <h2>Total budget: 250 millon</h2>
+            <h1>Make Your<span className="text"> Devloper  <FontAwesomeIcon icon={faCode} /></span> Team</h1>
+            <h3>Select your Devloper team and Start the project. Finish the teamwork before deadline within the budget.</h3>
+            <h2>Total budget : 10 millon</h2>
         </div>
     );
 };
