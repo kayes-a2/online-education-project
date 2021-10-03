1227da901369dfa0eb10858fb8ead5f60e866d97
Kayes-a2 <you@example.com>
2021-10-04 01:49:42 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-//import components & css file  
+import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header/Header';
-import Project from './components/Project/Project';
 
 function App() {
-
   return (
-    //header & project coponents
-    <div>
-      <Header></Header>
-      <Project></Project>
-
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
 
-//export default app file
 export default App;
